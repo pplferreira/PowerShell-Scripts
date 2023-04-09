75b51af43fae5cdd0de60e639590e09ba4f815b3
Pedro Paulo Ferreira <68658430+pplferreira@users.noreply.github.com>
2023-04-08 21:24:26 -0300

    Add files via upload

---
+++ PowerApps-Recovery.ps1
@@ -0,0 +1,40 @@
+﻿#Limpa a tela do prompt
+Clear
+
+#Conecta a conta da Power Platform
+Add-PowerAppsAccount
+
+#Lista todos os ambientes
+Get-AdminPowerAppEnvironment
+
+#Adiciona na variavel o ambiente desejado
+$a = Read-Host "Qual ambiente deseja verificar? (EnvironmentName)"
+
+#Limpa tela
+Clear
+
+#Printa o ambiente selecionado
+Write-Host "Ambiente selecionado: " $a
+
+#Printa tela de load
+Write-Host "Iremos listar todos os projetos deletados....aguarde!"
+
+
+Get-AdminDeletedPowerAppsList -EnvironmentName $a
+
+$c = (Get-AdminDeletedPowerAppsList -EnvironmentName $a).Count
+
+
+#Adiciona na variavel ref app deletado
+$app = Read-Host "Foram encontrados $c app deletados, qual app deseja recuperar? (AppName)"
+
+
+#Recupera app
+Get-AdminRecoverDeletedPowerApp -AppName $app -EnvironmentName $a
+
+
+#Mensagem final
+Write-Host "O app foi restaurado com sucesso!"
+
+
+
---
+++ PowerBI-ExportMassive.ps1
@@ -0,0 +1,34 @@
+﻿#Limpa a tela do prompt
+Clear
+
+#Conecta ao Servico do Power BI
+Connect-PowerBIServiceAccount
+
+#Declarar Variavel
+$w = Read-Host "Qual o nome do Workspace?"
+
+#Get Workspaces
+
+$Wid = (Get-PowerBIWorkspace -Name $w).Id
+
+#Get Reports
+
+$Rid = Get-PowerBIReport -WorkspaceId $Wid
+
+#Data Atual
+
+$d = Get-Date -Format "dd-MM-yyyyHHmm"
+
+
+#Estrutura de Repeticao
+
+Foreach($i in $Rid ){
+
+$dir = "./" + $i.Name.Replace(":","-") + "_" + $d + ".pbix"
+
+Write-Host "O arquivo " $i.Name " Esta sendo exportado"
+
+Export-PowerBIReport -Id $i.Id -OutFile $dir
+
+
+}
